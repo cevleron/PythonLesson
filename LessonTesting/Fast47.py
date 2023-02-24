8e3673534966b423f828170ac936a0d2934ee460
Anton <Cevleron@yandex.ru>
2023-02-24 19:39:37 +0300

    Добавил описание действий

--- LessonTesting/Fast47.py
+++ LessonTesting/Fast47.py
@@ -44,7 +44,8 @@ result = [1, 5, 7, 3, 7]
 
 result = [1, 5, 7, 3, 7]
 new_list_1 = list(map(lambda num: num*num, result))
-new_list_2 = [item for item in result if item > 4]
+#           что сделать   где взять     при каком условии
+new_list_2 = [item*item for item in result if item > 4]
 print (new_list_2)
 
 
