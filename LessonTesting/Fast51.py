547267680da3d03aad31930d17d7ebd0362ac8ab
Anton <Cevleron@yandex.ru>
2023-02-25 11:01:31 +0300

    Задача 51

---
+++ LessonTesting/Fast51.py
@@ -0,0 +1,28 @@
+# Задача No51. Решение в группах
+# Напишите функцию same_by(characteristic, objects), которая проверяет, 
+# все ли объекты имеют одинаковое значение некоторой характеристики,
+# и возвращают True, если это так. Если значение характеристики для 
+# разных объектов отличается - то False. Для пустого набора объектов, 
+# функция должна возвращать True. Аргумент characteristic - это функция, 
+# которая принимает объект и вычисляет его характеристику.
+
+# Ввод:
+
+# values = [0, 2, 10, 6]
+# if same_by(lambda x: × % 2, values):
+#     print('same')
+# else:
+#     print('different')
+
+# Вывод:
+# same
+
+def same_by(characteristic, objects):
+    return len(set(map(characteristic, objects))) == 1
+
+
+values = [0, 2, 10, 6]
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
